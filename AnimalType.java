df5741b0ad215f8f46dcf9d5ca68d480232c44cf
Nikola <79028787+nikchas@users.noreply.github.com>
2021-02-14 02:23:17 +0200

    Add files via upload

---
+++ AnimalType.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
